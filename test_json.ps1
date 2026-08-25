1f3ef1ed6f2871a79196188c8bb4a792d6d5f461
Neulyomi <seongbeom150625@gmail.com>
2026-08-25 21:47:06 +0900

    Delete test_json.ps1

--- test_json.ps1
+++
@@ -1,4 +0,0 @@
-$path = "c:\Users\jl_rb\Documents\antigravity\intelligent-chandrasekhar\street_dna\data\streets.geojson"
-$content = [System.IO.File]::ReadAllText($path, [System.Text.Encoding]::UTF8)
-$json = $content | ConvertFrom-Json
-Write-Host "JSON IS 100% VALID. TOTAL FEATURES: " $json.features.Count
